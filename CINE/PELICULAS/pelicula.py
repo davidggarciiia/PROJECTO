6ade1a5075fc57bceb20bf924cc47825034e66eb
Alro4503 <alvarorodriguezl925@gmail.com>
2025-10-08 19:25:38 +0200

    feat(movie class): Movie class and its atributes created

---
+++ CINE/PELICULAS/pelicula.py
@@ -0,0 +1,8 @@
+class Pelicula:
+    def __init__(self, titulo,director,año,horario):
+        self.titulo = titulo
+        self.horario = horario
+        self.director = director
+        self.año = año
+    
+    
