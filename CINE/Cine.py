3dd06cc3318e4ef28a34dcba87d570e0444fd26b
David <you@example.com>
2025-10-17 18:08:51 +0200

    feat(changes in ticket)

--- CINE/Cine.py
+++ CINE/Cine.py
@@ -1,19 +1,21 @@
 from ASIENTOS import asientos
-
+from TICKET.ticket import Ticket  # Importar la clase Ticket
 
 print("---BIENVENIDO AL CINEBICHO---")
 
 asientos.mostrar_asientos()
 
+asientos_seleccionados = []
 
 while True:
     asiento_propuesto = asientos.proponer_mejor_asiento()
     print(f"Se le propone el asiento más centrado disponible: Fila {asiento_propuesto[0]}, Columna {asiento_propuesto[1]}")
-    
+
     fila = int(input("Ingrese el número de fila: "))
     columna = int(input("Ingrese el número de columna: "))
     
-    asientos.seleccionar_asiento(fila, columna)
+    if asientos.seleccionar_asiento(fila, columna):
+        asientos_seleccionados.append((fila, columna))  # Agregar el asiento seleccionado a la lista
     
     respuesta = input("\n¿Desea comprar otro asiento para esta película? (s/n): ").lower()
     
@@ -22,5 +24,9 @@ while True:
     
     print("\n")
     asientos.mostrar_asientos()
+
+# Crear y generar el ticket
+ticket = Ticket(pelicula, asientos_seleccionados, precio_total)
+ticket.generar_ticket()
 
 print("\n¡Gracias por su compra!")
